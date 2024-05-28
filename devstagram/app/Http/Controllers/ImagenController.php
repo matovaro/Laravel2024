37271b7aa246d2487b8c9001d1df4966c94da8b2
Miguel Tovar <matovaro@unal.edu.co>
2024-05-28 00:34:08 -0500

    Avances en cargue de imagenes

--- devstagram/app/Http/Controllers/ImagenController.php
+++ devstagram/app/Http/Controllers/ImagenController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
-    public function store(){
-        return 'Desde producción';
+    public function store(Request $request)
+    {
+        $imagen = $request->file('file');
+
+        $txtNombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000);
+
+        $imagenPath = public_path('uploads') . "/" . $txtNombreImagen;
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $txtNombreImagen]);
     }
 }
--- devstagram/resources/js/app.js
+++ devstagram/resources/js/app.js
@@ -11,3 +11,17 @@ const dropzone = new Dropzone('#dropzone', {
     maxFiles: 1,
     uploadMultiple: false
 });
+
+dropzone.on('sending', function(file, xhr, formData){
+    console.log('Imprimiendo info...');
+    console.log(file);
+});
+
+dropzone.on('success', function(file, response){
+    // Este response viene del imagenes.store
+    console.log(response);
+});
+
+dropzone.on('error', function(file, message){
+    console.log(message);
+});
